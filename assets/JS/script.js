e1692da4fffb1adab1d50c014743e92d96dad4ac
Wei Ming Tay <ming23.tay@gmail.com>
2022-11-20 21:40:00 +0100

    media

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,22 +1,27 @@
 // Variablen
-let button = document.querySelector("button");
+let button = document.querySelector("#button1");
+let entfernen = document.querySelector("#button2");
 let zahlenreihe = document.getElementById("zahlenreihe");
 let counter = 0;
 button.addEventListener("click", function zahlen(e) {
+	/* 	for (let i = 0; i < 100; i++) { */
 
+	let p = document.createElement("p");
+	p.textContent = counter;
 
-/* 	for (let i = 0; i < 100; i++) { */
-
-		let p = document.createElement("p");
-		p.textContent = counter;
-
-		if ((counter) % 10 == 0) {
-			p.style.backgroundColor = "red";
+	if (counter % 10 == 0) {
+		p.classList.add("firstNumber");
+		/* p.style.backgroundColor = "red";
 			p.style.color = "white";
 			p.style.borderRadius = "50%";
-		}
+			p.style.boxShadow="0 0 15px 2px red" */
+	}
 
-		zahlenreihe.appendChild(p);
-		counter++
-	
+	zahlenreihe.appendChild(p);
+	counter++;
+});
+entfernen.addEventListener("click", (e) => {
+	let ps = document.querySelectorAll("p");
+	console.log(ps);
+	ps.removeChild()
 });
